9d353bbc354a827913888c842b1354b063e2ed7a
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2025-05-31 20:56:11 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon({
-    include: {
-      mdi: ["*"]
-    }
-  })],
+  site: 'https://www.litografiaalbeiro.com',
+  trailingSlash: 'always',
+  integrations: [
+    icon({
+      include: {
+        mdi: ["*"]
+      }
+    }),
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()]
   },
